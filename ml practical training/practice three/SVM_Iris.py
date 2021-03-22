5b31f39074e6b656c9e89fc93c5c771c15e0e2f6
shuaihanghang-1998 <865284805@qq.com>
2021-03-22 12:03:44 +0800

    实验三

--- ml practical training/practice three/SVM_Iris.py
+++ ml practical training/practice three/SVM_Iris.py
@@ -2,7 +2,7 @@ from itertools import product
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn.model_selection import train_test_split
 from sklearn import datasets
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
